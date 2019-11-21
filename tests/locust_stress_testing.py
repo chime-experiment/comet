4d3825fc3475781403d912d5ed7fd9b551274751
anjakefala <anja.kefala@gmail.com>
2019-11-21 14:25:56 -0800

    docs: add docusts to locust stress testing

--- tests/locust_stress_testing.py
+++ tests/locust_stress_testing.py
@@ -1,3 +1,11 @@
+"""
+Code for running stress tests using locust.
+
+https://docs.locust.io/en/latest/installation.html
+
+To run tests, run `locust -f tests/locust_stress_testing.py` on the commandline.
+Code for the locust client is located in tests/DummyClient.py.
+"""
 import datetime
 import time
 import os
@@ -5,28 +13,23 @@ import json
 import shutil
 import signal
 import string
-import pytest
 import tempfile
 import random
-
-from locust import TaskSet, task
-from DummyClient import DummyClientLocust
-
 from subprocess import Popen
 
-CHIMEDBRC = os.path.join(os.getcwd(), ".chimedb_test_rc")
-CHIMEDBRC_MESSAGE = "Could not find {}.".format(CHIMEDBRC)
-PORT = "8000"
+import pytest
+from locust import TaskSet, task
 
+from DummyClient import DummyClientLocust
+
+
+CHIMEDBRC = os.path.join(os.getcwd(), ".chimedb_test_rc")
+PORT = "8000"
+HOST = "localhost"
 version = "0.1.1"
 
-large_data = []
-for i in range(2035):
-    for j in range(0, 2035):
-        large_data.append([i, j])
-
-
 def make_small_state():
+    """Build a small state for sample_data."""
     return {
         "state": {
             "time": str(datetime.datetime.utcnow()),
@@ -35,22 +38,51 @@ def make_small_state():
         "hash": random.getrandbits(40),
     }
 
+large_data = []
+for i in range(2035):
+    for j in range(0, 2035):
+        large_data.append([i, j])
+
 
 def make_large_state():
+    """Build a large state for sample_data."""
     state = {"state": {"inner": {"data": large_data}}, "hash": random.getrandbits(40)}
 
     return state
 
 
 class MyTasks(TaskSet):
+    """Set of tasks for testing comet broker.
+
+    To add a task, create a function and decorate it with @task(n)
+    where the higher n is the more frequently that task will be executed.
+
+    Attributes
+    ----------
+    client : class:DummyClient:
+        An instance of the DummyClient that represents one theoretical manager.
+    curr_base : str
+        The hash for the current base dataset of the dataset tree.
+        For /update-dataset to be more stressfull for comet,
+        we need to build dependencies between datasets: so we create one and
+        use it as a base dataset for the next one.
+    """
+
     def on_start(self):
+        """Run upon initialising each individual client."""
         # register root dataset
-        state_id = self.client.register_state({"foo": "bar"}, "test")
-        dset_id = self.client.register_dataset(state_id, None, "test", True)
-        self.curr_base = dset_id
+        root_state_id = self.client.register_state({"foo": "bar"}, "test")
+        root_dset_id = self.client.register_dataset(root_state_id, None, "test", True)
+        self.curr_base = root_dset_id
 
     @task(5)
     def register_small_dataset(self):
+        """
+        Register a small dataset with the CoMeT broker.
+
+        Associate it with the current base dataset.
+        It is now the new base dataset.
+        """
         state = make_small_state()
 
         state_id = self.client.register_state(state, "test")
@@ -59,6 +91,12 @@ class MyTasks(TaskSet):
 
     @task(1)
     def register_large_dataset(self):
+        """
+        Register a large dataset with the CoMeT broker.
+
+        Associate it with the current base dataset.
+        It is now the new base dataset.
+        """
         state = make_large_state()
 
         state_id = self.client.register_state(state, "test")
@@ -67,25 +105,45 @@ class MyTasks(TaskSet):
 
     @task(4)
     def update_dataset(self):
+        """Make a call to /update-dataset using a randomly chosen registered dataset."""
+
         if self.client.datasets:
             ds_id = random.choice(list(self.client.datasets))
             self.client.update_datasets(ds_id)
 
     @task(4)
     def request_state(self):
+        """Make a call to /request-state using a randomly chosen registered state."""
         if self.client.states:
             state_id = random.choice(list(self.client.states))
             self.client.request_state(state_id)
 
 
 class DummyManager(DummyClientLocust):
-    host = "localhost"
+    """
+    Configuration for the CoMeT locust tests.
+
+    Attributes
+    ----------
+    host : str
+        hostname for the CoMeT broker
+    port : str
+        port for the CoMeT broker
+    wait_time : func
+        each locust instance uses this to calculate how long to idle
+        after a particular request
+    task_set : :class:TaskSet
+        set of tasks that define the locusts behaviour
+    """
+
+    host = HOST
     port = PORT
     wait_time = lambda x: 3
     task_set = MyTasks
 
     def setup(self):
-        assert os.path.isfile(CHIMEDBRC), CHIMEDBRC_MESSAGE
+        """Set-up operations before any of the locusts start up."""
+        assert os.path.isfile(CHIMEDBRC), "Could not find {}.".format(CHIMEDBRC)
         os.environ["CHIMEDB_TEST_RC"] = CHIMEDBRC
 
         # Make sure that we don't write to the actual chime database
@@ -97,6 +155,7 @@ class DummyManager(DummyClientLocust):
         time.sleep(5)
 
     def teardown(self):
+        """Teardown operation at the very, very end of test execution."""
         pid = self.broker.pid
         os.kill(pid, signal.SIGINT)
         self.broker.terminate()
