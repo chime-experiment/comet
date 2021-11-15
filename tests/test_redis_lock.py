0d224613a1f08daaf33571574d66d24c1c519b3d
Adam Lanman <adamelanman@protonmail.com>
2021-11-14 19:44:19 -0500

    fix: aioredis syntax in unit tests
    fix: redis_async_locks for aioredis v2
    
    ci: use deprecated legacy resolver
    
    ci: remove cache_exceptions from alru_cache (no longer supported)
    
    ci: add redis healthcheck
    
    ci: install requirements for kotekan test

--- tests/test_redis_lock.py
+++ tests/test_redis_lock.py
@@ -1,3 +1,4 @@
+import os
 import asyncio
 import aioredis
 import pytest
@@ -8,19 +9,23 @@ logging.basicConfig(level=logging.DEBUG)
 
 from comet.redis_async_locks import Lock
 
+REDIS_PORT = os.environ.get("REDIS_PORT", 6379)
+REDIS_HOST = os.environ.get("REDIS_HOST", "0.0.0.0")
+REDIS_URL = "redis://{0}:{1}".format(REDIS_HOST, REDIS_PORT)
+
 
 @pytest.mark.asyncio
 async def test_lock():
     """Test that the locks work when called directly."""
 
-    redis = await aioredis.create_pool(("127.0.0.1", 6379), encoding="utf-8")
+    redis = aioredis.from_url(REDIS_URL, encoding="utf-8")
     lock = await Lock.create(redis, "test1")
 
     task1_end = False
 
     async def task1():
         r = await lock.acquire()
-        await r.execute("lpush", "testlist1", 1)
+        await r.lpush("testlist1", 1)
         # Block until told to end
         while not task1_end:
             await asyncio.sleep(0.05)
@@ -28,7 +33,7 @@ async def test_lock():
 
     async def task2():
         r = await lock.acquire()
-        assert await r.execute("del", "testlist1") == 1
+        assert await r.execute_command("del", "testlist1") == 1
 
     # Create and start up task1
     task1_future = asyncio.create_task(task1())
@@ -48,28 +53,30 @@ async def test_lock():
     assert task1_future.done()
     assert task2_future.done()
 
-    redis.close()
-    await redis.wait_closed()
+    await redis.close()
+
+
+#    await redis.wait_closed()
 
 
 @pytest.mark.asyncio
 async def test_lock_manager():
     """Test that the locks work when used as a context manager."""
 
-    redis = await aioredis.create_pool(("127.0.0.1", 6379), encoding="utf-8")
+    redis = aioredis.from_url(REDIS_URL, encoding="utf-8")
     lock = await Lock.create(redis, "test1")
 
     task1_end = False
 
     async def task1():
         async with lock as r:
-            await r.execute("lpush", "testlist1", 1)
+            await r.lpush("testlist1", 1)
             while not task1_end:
                 await asyncio.sleep(0.05)
 
     async def task2():
         async with lock as r:
-            assert await r.execute("del", "testlist1") == 1
+            assert await r.execute_command("del", "testlist1") == 1
 
     # Create and start up task1
     task1_future = asyncio.create_task(task1())
@@ -95,10 +102,12 @@ async def test_lock_manager():
 
     # Test that the lock entries are removed when it is closed
     await lock.close()
-    assert not await redis.execute("exists", lock.lockname)
+    assert not await redis.execute_command("exists", lock.lockname)
 
-    redis.close()
-    await redis.wait_closed()
+    await redis.close()
+
+
+#    await redis.wait_closed()
 
 
 @pytest.mark.asyncio
@@ -110,29 +119,27 @@ async def test_large():
 
     # Deliberately set a number of simultaneous connections much smaller than
     # the number of tasks to test connection starvation
-    redis = await aioredis.create_pool(("127.0.0.1", 6379), encoding="utf-8", maxsize=1)
-
+    redis = aioredis.from_url(REDIS_URL, encoding="utf-8")
     # Create the lock and reset the test value
     lock = await Lock.create(redis, key)
-    await redis.execute("del", key)
+    await redis.execute_command("del", key)
 
     # Perform a non-atomic increment of a variable in redis
     # This is obviously stupid except for testing the locking
     async def task():
         # with await redis as r:  ## Use this to see how badly the test fails
         async with lock as r:
-            val = await r.execute("get", key)
+            val = await r.get(key)
             val = int(val) if val is not None else 0
             # Deliberately sleep to give other tasks a chance to break things
             # (if the lock wasn't here)
             await asyncio.sleep(0.01)
-            await r.execute("set", key, val + 1)
+            await r.set(key, val + 1)
 
     tasks = [task() for _ in range(ntask)]
     await asyncio.gather(*tasks)
 
-    val = await redis.execute("get", key)
+    val = await redis.get(key)
     assert int(val) == ntask
 
-    redis.close()
-    await redis.wait_closed()
+    await redis.close()
